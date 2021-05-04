ead06d037a811451414d2e9731d92497ee9f6dab
Stuart Bonnell <stuart.bonnell@sarasin.co.uk>
2021-05-04 18:31:32 +0100

    Slow progress

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,8 @@ import WfProgress from './components/wfprogress/wfprogress'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+
         <WfProgress />
-      </header>
     </div>
   );
 }
--- src/components/wfprogress/wfprogress.tsx
+++ src/components/wfprogress/wfprogress.tsx
@@ -1,24 +1,56 @@
-import React from "react";
-import * as sql from "mssql";
+import { Component } from "react";
+import axios from "axios";
 import "./wfprogress.css";
-import { sqlconfig } from "../../sqlconfig";
 
-async function connectToSqlServer() {
-  try {
-    const pool = new sql.ConnectionPool(sqlconfig);
-    await pool.connect()
-    const request = new sql.Request(pool);
-    const result = await request.query(`select * from mock_people`);
-    console.dir(result);
-  }
-   catch (err) {
-    console.error(err);
-  }
-}
+const SERVER_URL = process.env.REACT_APP_K2_URL;
+const HEADERS = {
+  method: "Get",
+  headers: { Accept: "application/json;odata=verbose" },
+  mode: "cors" as RequestMode,
+};
 
-function WfProgress() {
-  connectToSqlServer();
-  return <div className="WfProgress">Hello from WF PROGRESS </div>;
+type StepState = { steps: any[] };
+type StepProps = any;
+class WfProgress extends Component<StepProps, StepState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      steps: ["Hello from State"],
+    };
+  }
+
+  async workflowStages() {
+    try {
+      const resp = await axios.get(`${SERVER_URL}WorkFlowDetails`);
+      console.dir(resp.data.value);
+      this.setState({ steps: resp.data.value });
+    } catch (err) {
+      // Handle Error Here
+      console.error(err);
+    }
+  }
+
+
+  componentDidMount() {
+    this.workflowStages();
+  }
+
+  render() {
+    return (
+      <div className="steps-progressbar">
+        Hello from WF PROGRESS
+        <ul>
+          {this.state.steps.sort((a, b) => a.StageId - b.StageId).map((Stage) => {
+            return (
+              <li className={Stage.StageClass}>
+                {Stage.StageId} - {Stage.StageName}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default WfProgress;
